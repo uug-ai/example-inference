c50e6c3c2b6c1b08bd7cb5b260fc1a22c75b2ab4
Cedric Verstraeten <cedric@verstraeten.io>
2024-05-30 17:28:21 +0200

    Update example_inference.py

--- example_inference.py
+++ example_inference.py
@@ -7,28 +7,31 @@ from dotenv import load_dotenv
 import os
 
 
-
 # Load environment variables
 load_dotenv()
 
 # Initialize a message broker using the python_queue_reader package
 print("1) Initializing RabbitMQ...")
-rabbitmq = RabbitMQ(queue_name = os.getenv('QUEUE_NAME'), 
-                    target_queue_name = os.getenv('TARGET_QUEUE_NAME'), 
-                    exchange = os.getenv('EXCHANGE'), 
-                    host = os.getenv('HOST'), 
-                    username = os.getenv('USERNAME'),
-                    password = os.getenv('PASSWORD'))
+rabbitmq = RabbitMQ(queue_name=os.getenv('QUEUE_NAME'),
+                    target_queue_name=os.getenv('TARGET_QUEUE_NAME'),
+                    exchange=os.getenv('EXCHANGE'),
+                    host=os.getenv('HOST'),
+                    username=os.getenv('USERNAME'),
+                    password=os.getenv('PASSWORD'))
 
 # Initialize Kerberos Vault
 print("2) Initializing Kerberos Vault...")
-kerberos_vault = KerberosVault(storage_uri = os.getenv('STORAGE_URI'),
-                               storage_access_key = os.getenv('STORAGE_ACCESS_KEY'),
-                               storage_secret_key = os.getenv('STORAGE_SECRET_KEY'))
+kerberos_vault = KerberosVault(storage_uri=os.getenv('STORAGE_URI'),
+                               storage_access_key=os.getenv(
+                                   'STORAGE_ACCESS_KEY'),
+                               storage_secret_key=os.getenv(
+                                   'STORAGE_SECRET_KEY'))
 
 
 # For educational purposes, we will receive 5 messages from the queue.
-# In a real-world scenario, the while loop would be used to continuously receive messages.
+# In a real-world scenario, the while loop would be used to continuously
+# receive messages.
+
 # while True:
 for _ in range(5):
     # Receive message from the queue
@@ -37,17 +40,17 @@ for _ in range(5):
 
     # Retrieve media from the Kerberos Vault
     print("4) Retrieving media from the Kerberos Vault...")
-    resp = kerberos_vault.retrieve_media(message, media_type = 'video', media_savepath = 'assets/videos/video.mp4')
-
+    resp = kerberos_vault.retrieve_media(
+        message, media_type='video', media_savepath='assets/videos/video.mp4')
 
     ############################################
-    ###    Implement your own logic here     ###
+    # Implement your own logic here
     ############################################
 
-
-    # In this example, we will perform color prediction on the first frame of the video
+    # In this example, we will perform color prediction on the first frame of
+    # the video
     print("5) Perform action on the media... (in this case color prediction)")
-    main_colors, _, _ = ColorPrediction.find_main_colors(image = read_first_frame('assets/videos/video.mp4'),
+    main_colors, _, _ = ColorPrediction.find_main_colors(image=read_first_frame('assets/videos/video.mp4'),
                                                          min_clusters=1,
                                                          max_clusters=5,
                                                          downsample_factor=0.95,
